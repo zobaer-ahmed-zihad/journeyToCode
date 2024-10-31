780dc0efdbd793927a22305e5a4fedb89318319c
zobaer ahmed zihad <zobaerahmedzihad02@gmail.com>
2024-10-31 14:35:49 +0600

    Solve counting letter problem

---
+++ Introduction_to_programming_language/11-String Operation and Frequency array/count_letters.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char st[200];
+    scanf("%s", &st);
+
+    int count[26] = {0};
+    int len = strlen(st);
+
+    for(int i = 0; i<len; i++){
+        count[st[i]-97]++;
+    }
+
+    for(int i = 0; i<26; i++){
+        if(count[i] != 0){
+            printf(" %c : %d \n", i+97, count[i]);
+        }
+    }
+    return 0;
+}
