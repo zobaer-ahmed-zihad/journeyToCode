5e41cc25bc23f3ba359c68ac2b7da631efae4bcf
zobaer ahmed zihad <zobaerahmedzihad02@gmail.com>
2024-11-09 11:33:52 +0600

    Basic input output in c++

---
+++ Introduction to C++/Basic C++/input_output.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Hello World!"<<endl;
+    cout<<"My name is zihad."<<endl<<"I live in Dhaka"<<endl;
+    int a;
+    cin>>a;
+    cout<<"Number is - "<<a;
+
+    return 0;
+}
