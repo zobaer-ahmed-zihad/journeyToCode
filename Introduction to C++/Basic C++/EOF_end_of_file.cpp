a8939389c0bdb902064e37773445b5573df30bc2
zobaer ahmed zihad <zobaerahmedzihad02@gmail.com>
2024-11-17 13:16:07 +0600

    EOF- End of file

---
+++ Introduction to C++/Basic C++/EOF_end_of_file.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+//    EOF - End of file 
+   int a;
+   while(cin>>a){
+    cout<<a<<endl;
+   }
+
+// setprecision
+   double x = 9.32547;
+   cout<<fixed<<setprecision(2)<<x;
+
+   return 0;
+}
