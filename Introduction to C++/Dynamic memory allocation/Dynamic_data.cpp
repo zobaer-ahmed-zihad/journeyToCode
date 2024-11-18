ff139eaff211f33ebb5bfa991bee7d1b0ddb095f
zobaer ahmed zihad <zobaerahmedzihad02@gmail.com>
2024-11-19 00:25:15 +0600

    Create dynamic variable in heap memory

---
+++ Introduction to C++/Dynamic memory allocation/Dynamic_data.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+   int *a = new int;
+   *a = 10;
+   cout<<*a<<endl;
+   float *num = new float;
+   *num = 7.3695;
+
+   cout<<*num<<endl;
+
+   return 0;
+}
