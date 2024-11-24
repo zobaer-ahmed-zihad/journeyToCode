c9b76f0943fe27aeeb1a635a49954afc1bcda329
zobaer ahmed zihad <zobaerahmedzihad02@gmail.com>
2024-11-24 18:45:23 +0600

    String in C++

---
+++ Introduction to C++/String in C++/string.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+   string st;
+   st = "Hello zihad";
+   string st2 = "Hello zihad";
+
+   if(st == st2){
+    cout<<"Same";
+   }else{
+    cout<<"Not same";
+   }
+//    cout<<st;
+
+   return 0;
+}
