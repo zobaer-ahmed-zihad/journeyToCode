abb51a0e816e757a9da2155502c2ba6510fa8615
zobaer ahmed zihad <zobaerahmedzihad02@gmail.com>
2024-11-24 22:07:48 +0600

    Element access in C++

---
+++ Introduction to C++/String in C++/Element_access.cpp
@@ -0,0 +1,15 @@
+// S[i] -> access the ith index of the string.
+// s.at(i) -> access the ith index of the string.
+// s.back() -> access the last element of the string.
+// s.front() -> access the first element of the string.
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+   string st = "Hello I am zihad";
+    // cout<<st[4]<<endl;
+    // cout<<st.at(6)<<endl;
+    cout<<st.back()<<endl;
+    cout<<st.front();
+
+   return 0;
+}
