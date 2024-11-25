aa0883a6160b45f0bfefe7747c527b684668ec54
zobaer ahmed zihad <zobaerahmedzihad02@gmail.com>
2024-11-25 19:48:08 +0600

    String constructor in C++

---
+++ Introduction to C++/More about String & Object/string_constructor.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+//    string s = "Hello world";
+    // 1st way
+    // string s("Hello world");
+    //Second way
+    // string s("Hello world", 5);
+    //Third way
+    string s = "Hello world";
+    string t(s, 5);
+   cout<<t<<endl;
+
+   return 0;
+}
