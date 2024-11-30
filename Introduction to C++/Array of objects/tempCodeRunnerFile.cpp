049ebf3729c2bb6cb42c7d61e0c1616eec414ea6
zobaer ahmed zihad <zobaerahmedzihad02@gmail.com>
2024-11-30 12:21:47 +0600

    Sort Array of Objects using Selection Sort

---
+++ Introduction to C++/Array of objects/tempCodeRunnerFile.cpp
@@ -0,0 +1,7 @@
+for(int i = 0; i<2; i++){
+    //     for(int j = i + 1; j<3; j++){
+    //         if(a[i].marks > a[j].marks){
+    //             swap(a[i], a[j]);
+    //         }
+    //     }
+    // }
