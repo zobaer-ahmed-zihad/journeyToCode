40dad48347345761692082d2619616f5a339904f
zobaer ahmed zihad <zobaerahmedzihad02@gmail.com>
2024-12-18 17:32:02 +0600

    Recap linked list head delete

---
+++ Data Structure/Singly Linked List Operations/Delete_head_recap.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int val;
+    Node *next;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+void delete_head(Node *&head)
+{
+    Node *DeleteHead = head;
+    head = head ->next;
+    delete DeleteHead;
+    cout<<endl<<"Head deleted"<<endl;
+}
+void print_linked_list(Node *head)
+{
+    Node *tmp = head;
+    while (tmp != NULL)
+    {
+        cout << tmp->val << " ";
+        tmp = tmp->next;
+    }
+    cout << endl
+         << endl;
+}
+int main()
+{
+    Node *head = new Node(10);
+    Node *a = new Node(20);
+    Node *b = new Node(30);
+
+    head->next = a;
+    a->next = b;
+
+    delete_head(head);
+    print_linked_list(head);
+
+    return 0;
+}
