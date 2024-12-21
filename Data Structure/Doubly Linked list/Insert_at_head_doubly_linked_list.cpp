28a28fa3fe0cf0b65b5ff0cc5e4a8b5171a703a7
zobaer ahmed zihad <zobaerahmedzihad02@gmail.com>
2024-12-21 23:48:07 +0600

    Insert at head doubly linked list

---
+++ Data Structure/Doubly Linked list/Insert_at_head_doubly_linked_list.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node *prev;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
+void print_linked_list_forward(Node *head)
+{
+    Node *tmp = head;
+    while (tmp != NULL)
+    {
+        cout << tmp->val << " ";
+        tmp = tmp->next;
+    }
+}
+void print_linked_list_reverse(Node *tail)
+{
+    Node *tmp = tail;
+    while (tmp != NULL)
+    {
+        cout << tmp->val << " ";
+        tmp = tmp->prev;
+    }
+}
+// Insert at head
+void insert_at_head(Node *&head, int v)
+{
+    Node *newNode = new Node(v);
+    newNode->next = head;
+    head->prev = newNode;
+    head = newNode;
+}
+int main()
+{
+
+    Node *head = new Node(10);
+    Node *a = new Node(20);
+    Node *tail = new Node(30);
+
+    head->next = a;
+
+    a->prev = head;
+    a->next = tail;
+
+    tail->prev = a;
+
+    insert_at_head(head, 100);
+    print_linked_list_forward(head);
+    return 0;
+}
