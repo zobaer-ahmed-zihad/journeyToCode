88f831bd074a9f3d74d80c48820db3bd01a13c15
zobaer ahmed zihad <zobaerahmedzihad02@gmail.com>
2024-12-23 21:15:08 +0600

    Delete any position - Doubly linked list

---
+++ Data Structure/Doubly Linked list/Delete_any_position_doubly_linked_list.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node *prev;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
+// print linked list
+void print_linked_list_forward(Node *head)
+{
+    Node *tmp = head;
+    while (tmp != NULL)
+    {
+        cout << tmp->val << " ";
+        tmp = tmp->next;
+    }
+}
+// Delete any position
+void delete_any_position(Node *head, int pos)
+{
+    Node *tmp = head;
+    for (int i = 1; i <= pos - 1; i++)
+    {
+        tmp = tmp->next;
+    }
+    Node *deleteNode = tmp->next;
+    tmp->next = tmp->next->next;
+    tmp->next->prev = tmp;
+    delete deleteNode;
+}
+int main()
+{
+
+    // Node *head = NULL;
+    // Node *tail = NULL;
+    Node *head = new Node(10);
+    Node *a = new Node(20);
+    Node *tail = new Node(30);
+
+    head->next = a;
+
+    a->prev = head;
+    a->next = tail;
+
+    tail->prev = a;
+
+    delete_any_position(head, 1);
+    print_linked_list_forward(head);
+    return 0;
+}
