79b1989ea1f6feaa29361fa8fec7cc59a13a87ac
zobaer ahmed zihad <zobaerahmedzihad02@gmail.com>
2024-12-24 21:22:22 +0600

    List element access and iterators

---
+++ Data Structure/STL List and Cycle Detection/List_element_access_and_iterators.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+   list<int>l = {10,20,30,40,50};
+
+    cout<<l.front()<<endl; // print head
+    cout<<l.back()<<endl; // print tail
+    cout<<*l.begin()<<endl; // print head
+   
+
+   return 0;
+}
