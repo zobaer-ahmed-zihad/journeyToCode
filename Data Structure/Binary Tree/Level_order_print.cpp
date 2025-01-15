bb4773e9c8cc36834552d1636efef3d91f4ac0c2
zobaer ahmed zihad <zobaerahmedzihad02@gmail.com>
2025-01-15 20:45:18 +0600

    Level order print - binary tree

---
+++ Data Structure/Binary Tree/Level_order_print.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int val;
+    Node *left;
+    Node *right;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+// level order traversing
+void level_order_print(Node *root)
+{
+    queue<Node *> q;
+    q.push(root);
+
+    while (!q.empty())
+    {
+        Node *f = q.front();
+        q.pop();
+        cout << f->val << " ";
+        if (f->left)
+            q.push(f->left);
+        if (f->right)
+            q.push(f->right);
+    }
+}
+
+int main()
+{
+    Node *root = new Node(1);
+    Node *a = new Node(2);
+    Node *b = new Node(3);
+    Node *c = new Node(4);
+    Node *d = new Node(5);
+    Node *e = new Node(6);
+    Node *f = new Node(7);
+
+    root->left = a;
+    root->right = b;
+    a->left = c;
+    a->right = d;
+    d->left = e;
+    d->right = f;
+
+    level_order_print(root);
+    return 0;
+}
