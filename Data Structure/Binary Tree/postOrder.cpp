48dac903e105930adc1849324e4058930203a055
zobaer ahmed zihad <zobaerahmedzihad02@gmail.com>
2025-01-15 19:06:59 +0600

    Post Order print - Binary tree

---
+++ Data Structure/Binary Tree/postOrder.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int val;
+    Node *left;
+    Node *right;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+void postOrder(Node *root)
+{
+    if (root == NULL)
+        return;
+    postOrder(root->left);
+    postOrder(root->right);
+    cout << root->val << " ";
+}
+int main()
+{
+    Node *root = new Node(1);
+    Node *a = new Node(2);
+    Node *b = new Node(3);
+    Node *c = new Node(4);
+    Node *d = new Node(5);
+    Node *e = new Node(6);
+    Node *f = new Node(7);
+
+    root->left = a;
+    root->right = b;
+    a->left = c;
+    a->right = d;
+    d->left = e;
+    d->right = f;
+
+    postOrder(root);
+
+    return 0;
+}
