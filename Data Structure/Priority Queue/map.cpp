cbf2b48112ecf7a78bbc23cd5d342ea0fddd308c
zobaer ahmed zihad <zobaerahmedzihad02@gmail.com>
2025-01-24 12:50:53 +0600

    Count using map

---
+++ Data Structure/Priority Queue/map.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+   map<string, int>mp;
+
+   mp["zihad"] = 7;
+   mp["Robik"] = 164;
+   mp["Khaled"] = 93;
+//    mp["rashed"] = 0;
+//    cout<<mp["zihad"]<<endl;
+
+    for(auto it = mp.begin(); it != mp.end(); it++)
+    {
+        cout<<it->first<<" "<<it->second<<endl;
+    }
+
+    // cout<<mp["hello"];
+    if(mp.count("rashed"))
+        cout<<"Ache";
+    else cout<<"Nai";
+
+   return 0;
+}
