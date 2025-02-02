04e40274315ea4efcfe7c2f21f5e7ec0f228ea0d
zobaer ahmed zihad <zobaerahmedzihad02@gmail.com>
2025-02-02 20:50:53 +0600

    Change file name

--- Algorithm/Graph/Adjacency_matrix_for_undirected_graph.cpp
+++ Algorithm/Graph/Adjacency_matrix_for_undirected_graph.cpp
@@ -2,14 +2,14 @@
 using namespace std;
 int main()
 {
-    int node, edge;
-    cin >> node >> edge;
 
-    int adj[node + 1][node + 1];
+    int n, e;
+    cin >> n >> e;
+    int adj[n][n];
 
-    for (int i = 0; i <= node; i++)
+    for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j <= node; j++)
+        for (int j = 0; j < n; j++)
         {
             adj[i][j] = 0;
             if (i == j)
@@ -18,18 +18,16 @@ int main()
             }
         }
     }
-    while (edge--)
+    while (e--)
     {
         int a, b;
         cin >> a >> b;
-
         adj[a][b] = 1;
         adj[b][a] = 1;
     }
-
-    for (int i = 0; i <= node; i++)
+    for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j <= node; j++)
+        for (int j = 0; j < n; j++)
         {
             cout << adj[i][j] << " ";
         }
