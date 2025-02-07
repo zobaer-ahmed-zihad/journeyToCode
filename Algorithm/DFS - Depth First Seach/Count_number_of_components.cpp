86419628cad3701ccbc7b3c826a1c21989e1906f
zobaer ahmed zihad <zobaerahmedzihad02@gmail.com>
2025-02-07 11:16:30 +0600

    Count number of components in a graph

---
+++ Algorithm/DFS - Depth First Seach/Count_number_of_components.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+int const N = 1e5 + 5;
+vector<int> adj_list[N];
+bool vis[N];
+
+void DFS(int src)
+{
+    // cout << src << " ";
+    vis[src] = true;
+
+    // for (int child : adj_list[src])
+    // {
+    //     if (!vis[child])
+    //         DFS(child);
+    // }
+
+    for (int i = 0; i < adj_list[src].size(); i++)
+    {
+        int child = adj_list[src][i];
+        if (!vis[child])
+            DFS(child);
+    }
+}
+int main()
+{
+    int n, e;
+    cin >> n >> e;
+    while (e--)
+    {
+        int a, b;
+        cin >> a >> b;
+        adj_list[a].push_back(b);
+        adj_list[b].push_back(a);
+    }
+    // DFS(0);
+    int count = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (!vis[i])
+        {
+            DFS(i);
+            count++;
+        }
+    }
+    cout << count << endl;
+
+    return 0;
+}
