15aee5458d93372002a23b47734bb5d1ec0ee63f
zobaer ahmed zihad <zobaerahmedzihad02@gmail.com>
2025-02-15 20:11:29 +0600

    Bellman Ford Algorithm

---
+++ Algorithm/Bellman Ford/Bellman_ford.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Edges
+{
+public:
+    int u;
+    int v;
+    int w;
+    Edges(int u, int v, int w)
+    {
+        this->u = u;
+        this->v = v;
+        this->w = w;
+    }
+};
+int dis[100];
+int main()
+{
+    int n, e;
+    cin >> n >> e;
+    vector<Edges> v;
+    while (e--)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+        Edges ed(a, b, c);
+        v.push_back(ed);
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        dis[i] = INT_MAX;
+    }
+    dis[1] = 0;
+    for (int i = 1; i <= n - 1; i++)
+    {
+        // for (int j = 0; j < v.size(); j++)
+        // {
+        //     Edges ed = v[j];
+        //     int u = ed.u;
+        //     int v = ed.v;
+        //     int w = ed.w;
+
+        //     if (dis[u] + w < dis[v])
+        //     {
+        //         dis[v] = dis[u] + w;
+        //     }
+        // }
+
+        for (Edges ed : v)
+        {
+            int u = ed.u;
+            int v = ed.v;
+            int w = ed.w;
+
+            if (dis[u] + w < dis[v])
+            {
+                dis[v] = dis[u] + w;
+            }
+        }
+    }
+
+    for (int i = 1; i <= n; i++)
+    {
+        cout << "Node " << i << " - " << dis[i] << endl;
+    }
+
+    return 0;
+}
