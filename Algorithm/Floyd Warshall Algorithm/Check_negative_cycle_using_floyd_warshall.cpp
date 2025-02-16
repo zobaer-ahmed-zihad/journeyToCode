9af49ee46a3a075b8431e3773d02c6bb3a9aedcd
zobaer ahmed zihad <zobaerahmedzihad02@gmail.com>
2025-02-16 21:52:54 +0600

    Chekc Negative cycle using Floyd Warshall

---
+++ Algorithm/Floyd Warshall Algorithm/Check_negative_cycle_using_floyd_warshall.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int INF = 1e7;
+int main()
+{
+    int n, e;
+    cin >> n >> e;
+    int dis[n + 5][n + 5];
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            dis[i][j] = INF;
+            if (i == j)
+            {
+                dis[i][j] = 0;
+            }
+        }
+    }
+    while (e--)
+    {
+        int u, v, w;
+        cin >> u >> v >> w;
+        dis[u][v] = w;
+    }
+    bool cycle = false;
+    for (int k = 1; k <= n; k++)
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= n; j++)
+            {
+                if (dis[i][j] < 0) // check negative cycle
+                    cycle = true;
+                if (dis[i][k] + dis[k][j] < dis[i][j])
+                {
+                    dis[i][j] = dis[i][k] + dis[k][j];
+                }
+            }
+        }
+    }
+
+    if (cycle)
+    {
+        cout << "Negative Cycle Detected";
+    }
+    else
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= n; j++)
+            {
+                if (dis[i][j] == INT_MAX)
+                    cout << "IN" << " ";
+                else
+                    cout << dis[i][j] << " ";
+            }
+            cout << endl;
+        }
+    }
+
+    return 0;
+}
