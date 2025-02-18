14cd8f27e296a5ba8183d6c6bb54551ad08bb3f4
zobaer ahmed zihad <zobaerahmedzihad02@gmail.com>
2025-02-18 21:23:23 +0600

    DSU union by Rank or level

--- Algorithm/Disjoint Set/Union_by_rank.cpp
+++ Algorithm/Disjoint Set/Union_by_rank.cpp
@@ -1,13 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 int parent[1000];
-int parent_size[1000];
+int parent_level[1000];
 void dsu_set(int n)
 {
     for (int i = 1; i <= n; i++)
     {
         parent[i] = -1;
-        parent_size[i] = 1;
+        parent_level[i] = 0;
     }
 }
 int dsu_find(int node)
@@ -25,31 +25,32 @@ void dsu_union(int a, int b)
 
     if (leaderA != leaderB)
     {
-        if (parent_size[leaderA] > parent_size[leaderB])
+        if (parent_level[leaderA] > parent_level[leaderB])
         {
             parent[leaderB] = leaderA;
-            parent_size[leaderA] += parent_size[leaderB];
+        }
+        else if (parent_level[leaderA] < parent_level[leaderB])
+        {
+            parent[leaderA] = leaderB;
         }
         else
         {
-            parent[leaderA] = leaderB;
-            parent_size[leaderB] += parent_size[leaderA];
+            parent[leaderB] = leaderA;
+            parent_level[leaderA]++;
         }
     }
 }
-
 int main()
 {
     int n, e;
     cin >> n >> e;
     dsu_set(n);
-
     while (e--)
     {
         int a, b;
         cin >> a >> b;
         dsu_union(a, b);
     }
-    cout << dsu_find(5);
+    cout << dsu_find(2);
     return 0;
 }
