0771a837a7f1572a728c49ee1ee983847453d66a
zobaer ahmed zihad <zobaerahmedzihad02@gmail.com>
2025-02-18 21:00:55 +0600

    DSU union by size

---
+++ Algorithm/Disjoint Set/Union_by_size.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+int parent[1000];
+int parent_size[1000];
+void dsu_set(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        parent[i] = -1;
+        parent_size[i] = 1;
+    }
+}
+int dsu_find(int node)
+{
+    while (parent[node] != -1)
+    {
+        node = parent[node];
+    }
+    return node;
+}
+void dsu_union(int a, int b)
+{
+    int leaderA = dsu_find(a);
+    int leaderB = dsu_find(b);
+
+    if (leaderA != leaderB)
+    {
+        if (parent_size[leaderA] > parent_size[leaderB])
+        {
+            parent[leaderB] = leaderA;
+            parent_size[leaderA] += parent_size[leaderB];
+        }
+        else
+        {
+            parent[leaderA] = leaderB;
+            parent_size[leaderB] += parent_size[leaderA];
+        }
+    }
+}
+
+int main()
+{
+    int n, e;
+    cin >> n >> e;
+    dsu_set(n);
+
+    while (e--)
+    {
+        int a, b;
+        cin >> a >> b;
+        dsu_union(a, b);
+    }
+    cout << dsu_find(4);
+    return 0;
+}
