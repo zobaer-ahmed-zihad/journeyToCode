fd5353ef5c4542d7ed336231d2efc5ac7175d7ca
zobaer ahmed zihad <zobaerahmedzihad02@gmail.com>
2025-03-06 09:14:13 +0600

    0-1 knapsack using recursion

---
+++ Algorithm/Dynamic Programming/0-1_knapsack_using_recursion.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+int knapsack(int n, int s, int v[], int w[])
+{
+    // base case
+    if (n == 0 || s == 0)
+        return 0;
+    if (w[n - 1] <= s)
+    {
+        int op1 = knapsack(n - 1, s - w[n - 1], v, w) + v[n - 1];
+        int op2 = knapsack(n - 1, s, v, w);
+        return max(op1, op2);
+    }
+    else
+    {
+        return knapsack(n - 1, s, v, w);
+    }
+}
+int main()
+{
+    /*
+    n = item
+    s = size
+    v = value
+    w = weight
+    */
+    int n;
+    cin >> n;
+    int v[n], w[n];
+
+    for (int i = 0; i < n; i++)
+        cin >> v[i];
+
+    for (int i = 0; i < n; i++)
+        cin >> w[i];
+
+    int s;
+    cin >> s;
+
+    int maxSize = knapsack(n, s, v, w);
+    cout << maxSize;
+
+    return 0;
+}
