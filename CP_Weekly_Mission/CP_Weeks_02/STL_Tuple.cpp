5b98541c97c228e3933485f24bdc7c56e5537f8a
zobaer ahmed zihad <zobaerahmedzihad02@gmail.com>
2025-04-08 17:09:49 +0600

    STL Tuple

--- CP_Weekly_Mission/CP_Weeks_02/STL_Tuple.cpp
+++ CP_Weekly_Mission/CP_Weeks_02/STL_Tuple.cpp
@@ -14,13 +14,27 @@ int main()
     // cout<<name<<" "<<roll<<" "<<phone;
 
     int n;
-    cin>>n;
-    tuple<string, int, char, string> students[n];
-    
+    cin >> n;
+    vector<tuple<string, int, char, int>> t;
+    for (int i = 0; i < n; i++)
+    {
+        string name;
+        int roll;
+        char id;
+        int phone;
 
-                            //TUPLE INTPUT ISSUE
+        cin >> name >> roll >> id >> phone;
+        t.push_back(make_tuple(name, roll, id, phone));
+    }
 
-
+    for (int i = 0; i < n; i++)
+    {
+        string name = get<0>(t[i]);
+        int roll = get<1>(t[i]);
+        char id = get<2>(t[i]);
+        int phone = get<3>(t[i]);
+        cout << name << " " << roll << " " << id << " " << phone<<endl;
+    }
 
     return 0;
 }
