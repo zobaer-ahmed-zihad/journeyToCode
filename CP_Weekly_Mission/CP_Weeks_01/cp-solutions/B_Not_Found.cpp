82a0ce297167ae550ac8e734219cf47bba47b7fa
zobaer ahmed zihad <zobaerahmedzihad02@gmail.com>
2025-04-09 11:48:47 +0600

    Solved Not Found - AtCodder Problem

--- CP_Weekly_Mission/CP_Weeks_01/cp-solutions/B_Not_Found.cpp
+++ CP_Weekly_Mission/CP_Weeks_01/cp-solutions/B_Not_Found.cpp
@@ -6,21 +6,26 @@ int main()
     cin.tie(nullptr);
     string st;
     cin >> st;
+    sort(st.begin(), st.end());
     int fre[26] = {0};
     for (int i = 0; i < st.size(); i++)
     {
         fre[st[i] - 97]++;
     }
-    bool flag = true;
-    for (int i = 0; i < st.size(); i++)
+    bool flag = false;
+    for (int i = 0; i <26; i++)
     {
         if (fre[i] == 0)
         {
             cout << char(i + 97);
+            flag = true;
             break;
         }
     }
-    
+    if (!flag)
+        cout << "None";
 
     return 0;
 }
+
+//Problem Link: https://atcoder.jp/contests/abc071/tasks/abc071_b?lang=en
