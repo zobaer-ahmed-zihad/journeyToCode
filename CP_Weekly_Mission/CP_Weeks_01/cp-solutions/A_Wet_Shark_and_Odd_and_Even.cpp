a4687ad20187da09e11afcc747c62067e999f11a
zobaer ahmed zihad <zobaerahmedzihad02@gmail.com>
2025-04-11 11:49:29 +0600

    Solved Wet Shark and Odd and Even - Codeforces problem

--- CP_Weekly_Mission/CP_Weeks_01/cp-solutions/A_Wet_Shark_and_Odd_and_Even.cpp
+++ CP_Weekly_Mission/CP_Weeks_01/cp-solutions/A_Wet_Shark_and_Odd_and_Even.cpp
@@ -4,16 +4,38 @@ int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+
     int n;
-    cin>>n;
-    int sum = 0; 
-    while(n--)
+    cin >> n;
+    long long int even_sum = 0;
+    long long int odd_sum = 0;
+    long long int ar[n];
+    vector<int> v;
+    for (int i = 0; i < n; i++)
     {
         int val;
-        cin>>val;
-        sum += val;
-    }
-    cout<<sum;
+        cin >> val;
+        if (val % 2 == 0)
+            even_sum += val;
 
+        if (val % 2 == 1)
+        {
+            odd_sum += val;
+            v.push_back(val);
+        }
+    }
+    int min_value;
+    if (!v.empty())
+        min_value = *min_element(v.begin(), v.end());
+
+    if (odd_sum % 2 == 0)
+        cout << even_sum + odd_sum;
+    else
+    {
+        odd_sum -= min_value;
+        cout << even_sum + odd_sum;
+    }
     return 0;
 }
+
+//problem link: https://codeforces.com/problemset/problem/621/A
