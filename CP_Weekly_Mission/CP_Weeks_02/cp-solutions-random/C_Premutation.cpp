cf6ba0a55dfa2d65317d558b06f93f24d437f7c3
zobaer ahmed zihad <zobaerahmedzihad02@gmail.com>
2025-04-16 16:00:09 +0600

    Solved Premutation problem

--- CP_Weekly_Mission/CP_Weeks_02/cp-solutions-random/C_Premutation.cpp
+++ CP_Weekly_Mission/CP_Weeks_02/cp-solutions-random/C_Premutation.cpp
@@ -1,29 +1,53 @@
 #include <bits/stdc++.h>
 using namespace std;
-void solve(int n)
-{
-    int ar[n - 1];
-    for (int i = 0; i < n - 1; i++)
-    {
-        cin >> ar[i];
-    }
-}
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-
     int t;
     cin >> t;
     while (t--)
     {
         int n;
         cin >> n;
+        vector<vector<int>> v(n, vector<int>(n - 1));
         for (int i = 0; i < n; i++)
         {
-            solve(n);
+            for (int j = 0; j < n - 1; j++)
+            {
+                int val;
+                cin >> val;
+                v[i][j] = val;
+            }
         }
-    }
 
+        deque<int> de;
+        int first = v[0][0];
+        for (int i = 1; i < n; i++)
+        {
+            if (first == v[i][0])
+            {
+                de.push_front(first);
+                break;
+            }
+            else
+            {
+                de.push_front(v[i][0]);
+            }
+        }
+        cout << de.front() << " ";
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n - 1; j++)
+            {
+                if (v[i][0] != de.front())
+                {
+                    cout << v[i][j] << " ";
+                }
+            }
+        }
+        cout << endl;
+    }
     return 0;
 }
+//problem link: https://codeforces.com/problemset/problem/1790/C
