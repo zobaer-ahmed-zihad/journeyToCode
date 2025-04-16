9e8944b7773e1540d263a2f814c4529f526abb10
zobaer ahmed zihad <zobaerahmedzihad02@gmail.com>
2025-04-16 18:32:13 +0600

    Solved Registration_system - Rating 1300

---
+++ CP_Weekly_Mission/CP_Weeks_02/cp-solutions-topicwise/C_Registration_system.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    map<string, int> mp;
+    while (t--)
+    {
+        string st;
+        cin >> st;
+        if (mp.find(st) != mp.end())
+        {
+            int cnt = mp[st];
+            mp.erase(st);
+            mp.insert({st, ++cnt});
+            cout << st << mp[st] << '\n';
+        }
+        else
+        {
+            mp.insert({st, 0});
+            cout << "OK" << '\n';
+        }
+    }
+
+    return 0;
+}
