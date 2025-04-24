5baec67d04eae88a79f88d888bc42eaed5983073
zobaer ahmed zihad <zobaerahmedzihad02@gmail.com>
2025-04-24 08:15:00 +0600

    Solved Epic Transformation - Rating 1400

--- CP_Weekly_Mission/CP_Weeks_03/cp-solutions-topicwise/D_Epic_Transformation.cpp
+++ CP_Weekly_Mission/CP_Weeks_03/cp-solutions-topicwise/D_Epic_Transformation.cpp
@@ -1,40 +1,50 @@
 #include <bits/stdc++.h>
 using namespace std;
-void solve()
-{
-    int n;
-    cin >> n;
-    set<int> s;
-    for (int i = 0; i < n; i++)
-    {
-        int val;
-        cin >> val;
-        s.insert(val);
-    }
-    int ans = n / 2;
-    if (n % 2 == 0)
-    {
-        if (ans >= s.size())
-        {
-            cout << '0' << "\n";
-        }
-        else 
-            cout<<"NO"<<'\n';
-    }
-    else 
-        cout<<"NO"<<'\n';
-}
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-
     int t;
     cin >> t;
     while (t--)
     {
-        solve();
+        int n;
+        cin >> n;
+        map<int, int> mp;
+        for (int i = 0; i < n; i++)
+        {
+            long long int val;
+            cin >> val;
+            mp[val]++;
+        }
+        priority_queue<int> pq;
+        for (auto [value, occurrence] : mp)
+            pq.push(occurrence);
+        while (!pq.empty())
+        {
+            if (pq.size() < 2)
+                break;
+            int x, y;
+            x = pq.top();
+            pq.pop();
+            y = pq.top();
+            pq.pop();
+            x--;
+            y--;
+            if (x > 0)
+                pq.push(x);
+            if (y > 0)
+                pq.push(y);
+        }
+        int cnt = 0;
+        while (!pq.empty())
+        {
+            cnt += pq.top();
+            pq.pop();
+        }
+        cout << cnt << '\n';
     }
 
     return 0;
 }
+//problem link: https://codeforces.com/contest/1506/problem/D
