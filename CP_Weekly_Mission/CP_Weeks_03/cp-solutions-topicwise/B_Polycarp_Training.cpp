8eeeb4649ebbe2cc6de91fdbed9f414a14b3ea3b
zobaer ahmed zihad <zobaerahmedzihad02@gmail.com>
2025-04-24 10:18:32 +0600

    Solved Polycart training - Rating 1000

--- CP_Weekly_Mission/CP_Weeks_03/cp-solutions-topicwise/B_Polycarp_Training.cpp
+++ CP_Weekly_Mission/CP_Weeks_03/cp-solutions-topicwise/B_Polycarp_Training.cpp
@@ -1,32 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-int main() {
+int main()
+{
    ios::sync_with_stdio(false);
    cin.tie(nullptr);
-
-   int t;
-   cin >> t;
+   int n;
+   cin >> n;
    multiset<int> ml;
-   for (int i = 0;i < t;i++) {
-      int n;
-      cin >> n;
-      ml.insert(n);
+   while (n--)
+   {
+      int val;
+      cin >> val;
+      ml.insert(val);
    }
-
-   int result = 0, problems = 1;
-   while (!ml.empty()) {
-      auto LB = ml.lower_bound(problems);
-      if (LB != ml.end()) {
-         result++;
+   int i = 1;
+   int dayCnt = 0;
+   while (!ml.empty())
+   {
+      if (ml.lower_bound(i) != ml.end())
+      {
+         auto LB = ml.lower_bound(i);
          ml.erase(LB);
+         dayCnt++;
+         i++;
       }
-      else {
+      else
          break;
-      }
-      problems++;
    }
-   cout << result << '\n';
+   cout << dayCnt << '\n';
+
    return 0;
 }
 //problem link: https://codeforces.com/problemset/problem/1165/B
