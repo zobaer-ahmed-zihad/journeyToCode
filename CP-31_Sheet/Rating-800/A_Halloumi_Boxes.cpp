a0fa0b3961281d7c28e35a25b811f606c0c01173
zobaer ahmed zihad <zobaerahmedzihad02@gmail.com>
2025-05-02 20:50:16 +0600

    Halloumi Boxes - Rating 800 - (CP-31 sheet)

---
+++ CP-31_Sheet/Rating-800/A_Halloumi_Boxes.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+bool comp(int a, int b)
+{
+    return a > b;
+}
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, k;
+        cin >> n >> k;
+        vector<ll> v(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> v[i];
+        }
+        if (is_sorted(v.begin(), v.end()) || is_sorted(v.begin(), v.end(), comp) && k > 1)
+        {
+            cout << "YES" << '\n';
+        }
+        else
+        {
+            (k > 1) ? cout << "YES" << '\n' : cout << "NO" << '\n';
+        }
+    }
+
+    return 0;
+}
+//problem link: https://codeforces.com/problemset/problem/1903/A
